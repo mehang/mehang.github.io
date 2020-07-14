78c4600fdf00ac692e8c5dc6b6780b35b3ce0374
mehang <mehang.rai007@gmail.com>
2020-07-13 23:37:13 -0500

    Created teplate for dialer

---
+++ phone.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+$("#content_dialer").show();
+$("#content_list").hide();
+$("#content_add").hide();
+$("#btn_dialer").css("background", "white");
+});
+
+$("#btn_dialer").click(function(){
+    $("#content_dialer").show();
+    $("#content_list").hide();
+    $("#content_add").hide();
+    $(this).css("background", "white");
+    $("#btn_contact_list").css("background", "#E6E6E6");
+    $("#btn_contact_list").css("background", "#E6E6E6");
+});
+$("#btn_contact_list").click(function(){
+    $("#content_dialer").hide();
+    $("#content_list").show();
+    $("#content_add").hide();
+    $("#btn_dialer").css("background", "#E6E6E6");
+    $(this).css("background", "white");
+    $("#btn_contact_add").css("background", "#E6E6E6");
+});
+$("#btn_contact_add").click(function(){
+    $("#content_dialer").hide();
+    $("#content_list").hide();
+    $("#content_add").show();
+    $("#btn_dialer").css("background", "#E6E6E6");
+    $("#btn_contact_list").css("background", "#E6E6E6");
+    $(this).css("background", "white");
+});
