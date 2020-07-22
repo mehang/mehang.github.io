b1decc5e69b251e2e8845e1c0c3b1ef4f13161e0
mehang <mehang.rai007@gmail.com>
2020-07-22 00:58:19 -0500

    Fixed the color issue in alert message

--- assignment4/blog.js
+++ assignment4/blog.js
@@ -1,14 +1,18 @@
 $("#comment_post").click(function(){
     let name = $("#comment_name").val();
     let comment = $("#comment_content").val();
-    if ((name==="") && (comment==+"")){
+    if ((name==="") && (comment==="")){
         $("#message-body").text("Please provide name and comment!");
+        $("#message-body").css("color", "red");
     } else if (name==="") {
         $("#message-body").text("Please provide name!");
+        $("#message-body").css("color", "red");
     } else if (comment==="") {
         $("#message-body").text("Please provide comment!");
+        $("#message-body").css("color", "red");
     } else {
         $("#message-body").text("Comment has been posted.");
+        $("#message-body").css("color", "limegreen");
     }
     document.getElementById("message").className="";
 });
