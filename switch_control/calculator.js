9abaf387d054d57343758457275bdd451e7d21bc
mehang <mehang.rai007@gmail.com>
2020-08-02 23:34:19 -0500

    Added switch control for calculator

---
+++ switch_control/calculator.js
@@ -0,0 +1,236 @@
+// this gives us the order of the buttons, which we can use to step through the buttons in various directions
+// since we know the layout, + 1 moves to the next item, -1 previous, +4 is one row down, -4 is one row up
+let buttonOrder = ["#button7","#button8","#button9","#buttonDivide","#button4","#button5","#button6",
+	"#buttonMultiply","#button1","#button2","#button3","#buttonAdd","#button0","#buttonClear","#buttonEquals",
+	"#buttonSubtract"];
+
+let rowIndex = 0;
+let rowOrder = ["row_one", "row_two", "row_three", "row_four"];
+let navItemOrder = [["#button7", "#button8", "#button9", "#buttonDivide", "#row_one"],
+	["#button4", "#button5", "#button6", "#buttonMultiply", "#row_two"],
+	["#button1", "#button2", "#button3", "#buttonAdd", "#row_three"],
+	["#button0", "#buttonClear", "#buttonEquals", "#buttonSubtract", "#row_four"]];
+
+let itemIndex = 0;
+let switchRows = true;
+let rowInterval;
+let itemInterval;
+const interval = 1000;
+
+$(document).ready(function() {
+	$("#"+rowOrder[rowIndex]).addClass( "activeRow" );
+	rowInterval = setInterval(navigateRowCircularly,interval);
+});
+
+function navigateRowCircularly() {
+	$("#"+rowOrder[rowIndex]).removeClass("activeRow");
+	rowIndex= (rowIndex + 1) % rowOrder.length;
+	$("#"+rowOrder[rowIndex]).addClass("activeRow");
+}
+
+function navigateRowButtons(){
+	$(navItemOrder[rowIndex][itemIndex]).removeClass("activeButton");
+	itemIndex = (itemIndex + 1) % navItemOrder[rowIndex].length;
+	$(navItemOrder[rowIndex][itemIndex]).addClass("activeButton");
+}
+
+function onSwitchClick() {
+	if (switchRows){
+		clearInterval(rowInterval);
+		$(navItemOrder[rowIndex][0]).addClass("activeButton");
+		itemInterval = setInterval(navigateRowButtons, interval);
+	} else {
+		clearInterval(itemInterval);
+		$(navItemOrder[rowIndex][itemIndex]).removeClass("activeButton");
+		if (navItemOrder[rowIndex][itemIndex] !== ("#"+rowOrder[rowIndex])) {
+			$(navItemOrder[rowIndex][itemIndex]).click();
+		}
+		rowInterval = setInterval(navigateRowCircularly, interval);
+		itemIndex=0;
+
+	}
+	switchRows = !switchRows;
+}
+
+
+$("#buttonSelect").click(onSwitchClick);
+
+document.addEventListener('keydown', function (event) {
+	// enter key
+	if (event.keyCode === 13) {
+		onSwitchClick();
+	}
+});
+
+// add the selected class to an item. you can pass this any jquery selector, such as #id or .class
+// calling this will de-select anything currently selected
+function selectItem(name) {
+	$("button").removeClass("cursor");
+	$(name).addClass("cursor")
+}
+
+// gets the currently selected item, and returns its #id
+// returns null if no item is selected
+// note that if multiple items are selected, this will only return the first
+// but you could rewrite this to return a list of items if you wanted to track multiple selections
+function getSelectedItem() {
+	selected = $(".cursor"); // this returns an array
+	if (selected.length == 0) {
+		return null;
+	}
+	else {
+		return "#" + selected.first().attr('id')
+	}
+}
+
+// the next four functions move the selected UI control
+// this uses the array buttonOrder to know the order of the buttons. so you could change buttonOrder
+// to change the order that controls are highlighted/
+// if no button is currently selected, such as when the page loads, this will select the first
+// item in buttonOrder (which is the 7 button)
+// selectNext: go to the right, wrapping around to the next row
+// selectPrevious: go to the left, wrapping around to the previous row
+// selectUp: select the item above
+// selectDown: select the item below
+
+function selectNext() {
+	selected = getSelectedItem()
+	if (selected == null) {
+		selectItem(buttonOrder[0]);
+	} else {
+		index = buttonOrder.indexOf(selected);
+		index = (index + 1) % buttonOrder.length;
+		selectItem(buttonOrder[index])
+	}
+}
+
+function selectPrevious() {
+	selected = getSelectedItem()
+	if (selected == null) {
+		selectItem(buttonOrder[0]);
+	} else {
+		index = buttonOrder.indexOf(selected);
+		index = (index - 1);
+		if (index < 0) index = buttonOrder.length + index
+		selectItem(buttonOrder[index])
+	}
+}
+
+function selectUp() {
+	selected = getSelectedItem()
+	if (selected == null) {
+		selectItem(buttonOrder[0]);
+	} else {
+		index = buttonOrder.indexOf(selected);
+		index = (index - 4);
+		if (index < 0) index = buttonOrder.length + index
+		selectItem(buttonOrder[index])
+	}
+}
+
+function selectDown() {
+	selected = getSelectedItem()
+	if (selected == null) {
+		selectItem(buttonOrder[0]);
+	} else {
+		index = buttonOrder.indexOf(selected);
+		index = (index + 4) % buttonOrder.length;
+		selectItem(buttonOrder[index])
+	}
+}
+
+// actuate the currently selected item
+// if no item is selected, this does nothing
+// if multiple items are selected, this selects the first
+function clickSelectedItem() {
+	whichButton = getSelectedItem();
+	if (whichButton != null) {
+		$(whichButton).click();
+	}
+}
+
+// this function responds to user key presses
+// you'll rewrite this to control your interface using some number of keys
+$(document).keypress(function(event) {
+	if (event.key == "a") {
+		alert("You pressed the 'a' key!")
+	} else if (event.key == "b") {
+		alert("You pressed the 'b' key!")
+	}
+})
+
+
+/* calculator stuff below here */
+// for operations, we'll save + - / *
+firstValue = null;
+operation = null;
+addingNumber = false;
+
+digits = "0123456789"
+operators = "+-*/"
+
+// handle calculator functions. all buttons with class calcButton will be handled here
+$(".calcButton").click(function(event) {
+	buttonLabel = $(this).text();
+	// if it's a number, add it to our display
+	if (digits.indexOf(buttonLabel) != -1) {
+		// if we weren't just adding a number, clear our screen
+		if (!addingNumber) {
+			$("#number_input").val("")
+		}
+		$("#number_input").val($("#number_input").val() + buttonLabel);
+		addingNumber = true;
+		// if it's an operator, push the current value onto the stack
+	} else if (operators.indexOf(buttonLabel) != -1) {
+		// have we added a number? if so, check our stack
+		if (addingNumber) {
+			// is this the first number on the stack?
+			// if so, save it
+			if (firstValue == null) {
+				firstValue = $("#number_input").val();
+				addingNumber = false;
+				// do we have a number on the stack already? if so, this is the same as equals
+			} else if (firstValue != null) {
+				secondValue = $("#number_input").val();
+				evaluateExpression(firstValue,operation,secondValue)
+				// in this case, keep the operation
+				firstValue = $("#number_input").val();
+				addingNumber = false;
+			}
+		}
+		// either way, save this as the most recent operation
+		operation = buttonLabel;
+	} else if (buttonLabel == "C") {
+		$("#number_input").val("");
+		firstValue = null;
+		operation = null;
+		addingNumber = false;
+	} else if (buttonLabel == "=") {
+		if (firstValue != null && operation != null && addingNumber) {
+			secondValue = $("#number_input").val();
+			evaluateExpression(firstValue,operation,secondValue);
+			// clear our state
+			firstValue = null;
+			operation = null;
+			addingNumber = true
+		}
+	}
+})
+
+// do the math for our calculator
+function evaluateExpression(first,op,second) {
+	output = 0;
+	if (op == "+") {
+		output = parseInt(first) + parseInt(second);
+	} else if (op == "-") {
+		output = parseInt(first) - parseInt(second);
+	} else if (op == "*") {
+		output = parseInt(first) * parseInt(second);
+	} else if (op == "/") {
+		output = parseInt(first) / parseInt(second);
+	}
+
+	// now, handle it
+	$("#number_input").val(output.toString());
+	// deal with state elsewhere
+}
